7dd448fe37619305188cf67d8a74a5a0ad83d632
Jhonier Santana <101937551+JhonierSantana@users.noreply.github.com>
2022-12-16 12:16:06 -0500

    Create minishell.h

---
+++ minishell.h
@@ -0,0 +1,20 @@
+#ifndef MINISHELL_H
+#define MINISHELL_H
+
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <stdarg.h>
+
+extern char **environ;
+
+char *_getenv(const char *name);
+int _strcmp(const char *s1, const char *s2);
+char *_strcpy(char *dest, char *src);
+
+#endif
