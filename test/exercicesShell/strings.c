a3f5a2f952ce5d2b3714402fdf19bd6b601b57a4
root <root@DESKTOP-ISQ43BP>
2022-12-19 10:48:17 -0500

    strings.c

--- test/exercicesShell/strings.c
+++ test/exercicesShell/strings.c
@@ -92,3 +92,18 @@ ssize_t _puts(char *str)
 	}
 	return (len);
 }
+
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	i = j = 0;
+	while (*(dest + i))
+		i++;
+	while ((*(dest + i) = *(src + j)))
+	{
+		i++;
+		j++;
+	}
+	return (dest);
+}
